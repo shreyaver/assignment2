06cb89c1767c138fc492048484779856b1bae3f9
Shreya Verma <svshreya97@gmail.com>
2019-02-01 11:54:26 +0530

    Migrations: Added migration to create table

---
+++ migrations/20190201054634-create-books.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      Author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      Name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      rating: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('books');
+  }
+};
