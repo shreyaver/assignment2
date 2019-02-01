94811e129216ee5653c37caa304270e4760876f1
Shreya Verma <svshreya97@gmail.com>
2019-02-01 11:56:11 +0530

    Model: Able to write to table

--- models/books.js
+++ models/books.js
@@ -9,5 +9,20 @@ module.exports = (sequelize, DataTypes) => {
     Name: DataTypes.STRING,
     rating: DataTypes.DECIMAL,
   }, {});
+  books.generate = bookObj => new Promise((resolve, reject) => {
+    books.findAll({ where: { id: bookObj.id } }).then((entry) => {
+      if (entry.length === 0) {
+        books.create(bookObj).then(() => {
+          resolve(`Inserted book with id: ${bookObj.id}`);
+        }).catch((errorObj) => {
+          resolve(errorObj.message);
+        });
+      } else {
+        resolve(`Book with id: ${bookObj.id} already exists`);
+      }
+    }).catch((errorObj) => {
+      resolve(errorObj.message);
+    });
+  });
   return books;
 };
